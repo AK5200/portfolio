50736d9f2a6303b76f79d3fb095171c749a4d3bf
Anupam <anupamkmr5200@gmail.com>
2024-07-01 22:10:41 +0530

    [Anupam] added: portfolio

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline( {scrollTrigger:{
+    trigger:"#main",
+    start: "50% 50%",
+    end: "100% 10%",
+    scrub:2,
+    pin:true,
+}});
+
+tl.
+to("#top",{
+    top:"-50%"
+},"a").
+to("#bottom",{
+    bottom:"-50%"
+},"a")
+
+
+var tl1 = gsap.timeline( {scrollTrigger:{
+    trigger:"#main2",
+    start: "50% 50%",
+    end: "100% 10%",
+    scrub:2,
+    pin:true,
+}});
+
+tl1.
+to("#top2",{
+    top:"-50%"
+},"b").
+to("#bottom2",{
+    bottom:"-50%"
+},"b")
+
+
+
+
+
